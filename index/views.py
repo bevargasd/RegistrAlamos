b6e17aaf59e27f282d512860145a8579303cb543
bevargasd <80842978+bevargasd@users.noreply.github.com>
2023-05-09 12:14:48 -0400

    Login funcional
    Falta agregar funcionalidades para darle utilidad al mismo

--- index/views.py
+++ index/views.py
@@ -1,5 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
+from .forms import UserRegisterForm
+from django.contrib import messages
 
-# Create your views here.
 def inicio(request):
     return render(request, 'index.html') 
+
+def login(request):
+    return render(request, 'login.html') 
+
+def home(request):
+    return render(request, 'home.html') 
+
+def registro(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            return redirect('home')
+
+    else: 
+        form = UserRegisterForm()
+    context = {'form': form}
+    return render(request, 'registro.html', context)
--- setup/settings.py
+++ setup/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'crispy_forms',
+    'crispy_bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,6 +42,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'index'
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -105,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-es'
 
 TIME_ZONE = 'UTC'
 
@@ -125,3 +131,6 @@ STATICFILES_DRIS = (os.path.join(BASE_DIR, 'index/static'),)
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_REDIRECT_URL = 'home'
